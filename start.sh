65b2c64a7b26201013ce9dc42ef1f853c60015c0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-23 20:27:06 +0200

    simple locally usable skeleton

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+MINETEST_VERSION=5.2.0
+
+docker run --rm -it \
+  -u root:root \
+	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf:ro \
+  -v $(pwd)/worldmods:/root/.minetest/worlds/world/worldmods \
+  --network host \
+	registry.gitlab.com/minetest/minetest/server:${MINETEST_VERSION}
