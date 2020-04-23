1c5f95516101028b39f5e3b3f076cc91e609e69e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-23 21:00:48 +0200

    update script / settings

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ MINETEST_VERSION=5.2.0
 docker run --rm -it \
   -u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf:ro \
-  -v $(pwd)/worldmods:/root/.minetest/worlds/world/worldmods \
+	-v $(pwd)/worldmods:/root/.minetest/worlds/world/worldmods \
+	-v $(pwd)/data:/root/.minetest/worlds/world \
   --network host \
 	registry.gitlab.com/minetest/minetest/server:${MINETEST_VERSION}
